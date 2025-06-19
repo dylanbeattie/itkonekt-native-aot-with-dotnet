e456e48dcbcb38d5ee7794af51e2e1c069eb6da6
Dylan Beattie <dylan@dylanbeattie.net>
2025-06-19 16:03:17 +0200

    Dockerfile for native Linux/AOT build

--- Songbook/Program.cs
+++ Songbook/Program.cs
@@ -1,7 +1,13 @@
 using Songbook;
 using Songbook.Data;
+using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options => {
+	options.SerializerOptions.TypeInfoResolverChain.Insert(0, SongbookSerializerContext.Default);
+});
+
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton(new Database());
 var app = builder.Build();
@@ -11,7 +17,14 @@ app.MapGet("/", Html.Index);
 
 app.MapGet("/artists", (Database db) => db.Artists);
 app.MapGet("/artists/{name}/tracks", (string name, Database db) => db.ListTracksByArtist(name));
-app.MapGet("/tracks", (Database db) => db.Artists.SelectMany(a => a.Tracks));
+app.MapGet("/tracks", (Database db) => db.Artists.SelectMany(a => a.Tracks).ToList());
 
 app.Run();
 
+[JsonSerializable(typeof(Artist))]
+[JsonSerializable(typeof(Track))]
+[JsonSerializable(typeof(List<Artist>))]
+[JsonSerializable(typeof(List<Track>))]
+internal partial class SongbookSerializerContext : JsonSerializerContext {
+
+}
